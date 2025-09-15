87dc75ecd527b2aab3d012354888d4ea4307c855
Cercedo <marloncercedo@gmail.com>
2025-09-15 18:06:07 -0500

    chore: format using double quotes

--- src/app/__test__/page.test.tsx
+++ src/app/__test__/page.test.tsx
@@ -1,10 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
 
-import Page from '../page';
+import Page from "../page";
 
-describe('Page', () => {
-  it('should render the text `Get started by editing`', () => {
+describe("Page", () => {
+  it("should render the text `Get started by editing`", () => {
     render(<Page />);
     expect(screen.getByText(/^Get started by editing/)).toBeDefined();
   });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,13 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vitest/config';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.ts(x)?'],
+    environment: "jsdom",
+    include: ["src/**/*.{test,spec}.ts(x)?"],
     coverage: {
-      include: ['src/**/*.ts', 'src/**/*.tsx'],
+      include: ["src/**/*.ts", "src/**/*.tsx"],
     },
   },
 });
